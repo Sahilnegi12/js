ffd0e56c7b85129345d1a9c03c7a2eff4392c9b1
Sahilnegi12 <168193226+Sahilnegi12@users.noreply.github.com>
2025-03-04 12:57:28 +0000

    String

---
+++ basics/String.js
@@ -0,0 +1,40 @@
+const  name = "sahil"
+const repoCount = 50
+
+//console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('sahil-sn')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('2'));
+// console.log(gameName.indexOf('h'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-7, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   sahil   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://sahil.com/sahil%20negi"
+console.log(url.replace('%20', "-"))
+
+console.log(url.includes('snake'))
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
