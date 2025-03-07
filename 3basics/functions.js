615529ca68042f01a2c05e228b044d0465e666de
Sahilnegi12 <168193226+Sahilnegi12@users.noreply.github.com>
2025-03-07 05:45:04 +0000

    functions

--- 3basics/functions.js
+++ 3basics/functions.js
@@ -26,16 +26,45 @@ function sayMyName(){
 
 // console.log("Result: ", result);
 
-function loginUserMessage(username = "sam"){
-    if(!username){
-        console.log("Please enter a username");
-        return 
+// function loginUserMessage(username = "sam"){
+//     if(!username){
+//         console.log("Please enter a username");
+//         return 
 
-    }
-    return `${username} just logged in`
+//     }
+//     return `${username} just logged in`
+// }
+
+
+// //console.log(loginUserMessage("Sahil"));
+// console.log(loginUserMessage());
+
+function calculateCartPrice (val1, val2, ...num1){
+    return num1
 }
 
+console.log(calculateCartPrice(200, 400, 500, 2000));
 
-//console.log(loginUserMessage("Sahil"));
-console.log(loginUserMessage());
+const user = {
+    username : "hitesh",
+    price: 199
+}
 
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+//handleObject(user)
+handleObject({
+    username:"sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
